35c96992285ef99d9371a9c6d10f551123180a2d
map_map_bear <37301000+mapmapbear@users.noreply.github.com>
2022-08-31 00:39:54 +0800

    Learn Session 1 Finish

---
+++ Custom RP/Runtime/CameraRenderer.Editor.cs
@@ -0,0 +1,76 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Profiling;
+
+namespace SRPStudy
+{ 
+    partial class CameraRenderer
+    {
+        partial void DrawUnsupportedShaders();
+        partial void DrawGizmos();
+        partial void PrepareForSceneWindow();
+        partial void PrepareBuffer();
+#if UNITY_EDITOR
+        private static Material errorMaterial;
+        private static ShaderTagId[] legacyShaderTagIds =
+        {
+            new ShaderTagId("Always"),
+            new ShaderTagId("ForwardBase"),
+            new ShaderTagId("PrepassBase"),
+            new ShaderTagId("Vertex"),
+            new ShaderTagId("VertexLMRGBM"),
+            new ShaderTagId("VertexLM")
+        };
+#endif
+        
+#if UNITY_EDITOR
+        partial void DrawUnsupportedShaders()
+        {
+            if (errorMaterial == null)
+            {
+                errorMaterial = new Material(Shader.Find("Hidden/InternalErrorShader"));
+            }
+            // 把支持的shader类型数组插入到过滤内容中
+            var drawingSettings = new DrawingSettings(legacyShaderTagIds[0], new SortingSettings(_camera)
+            ){
+                overrideMaterial = errorMaterial
+            };
+            for (int i = 1; i < legacyShaderTagIds.Length; ++i) {
+                drawingSettings.SetShaderPassName(i, legacyShaderTagIds[i]);
+            }
+            
+            var filteringSettings = FilteringSettings.defaultValue;
+            _context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
+        }
+
+        partial void DrawGizmos()
+        {
+            if (Handles.ShouldRenderGizmos())
+            {
+                _context.DrawGizmos(_camera,GizmoSubset.PreImageEffects);
+                _context.DrawGizmos(_camera, GizmoSubset.PostImageEffects);
+            }
+        }
+
+        partial void PrepareForSceneWindow()
+        {
+            if (_camera.cameraType == CameraType.SceneView)
+            {
+                ScriptableRenderContext.EmitGeometryForCamera(_camera);
+            }
+        }
+
+        private string SampleName { get; set; }
+        partial void PrepareBuffer()
+        {
+            Profiler.BeginSample("Editor Only");
+            buffer.name = SampleName = _camera.name;
+            Profiler.EndSample();
+        }
+#else
+        const string SampleName = bufferName;
+        
+#endif
+    }
+}
--- Custom RP/Runtime/CameraRenderer.cs
+++ Custom RP/Runtime/CameraRenderer.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering;
 
+
 namespace SRPStudy
 { 
-    public class CameraRenderer
+    partial class CameraRenderer
     {
         private ScriptableRenderContext _context;
         private Camera _camera;
@@ -13,33 +14,53 @@ namespace SRPStudy
             name = bufferName
         };
         private CullingResults _cullingResults;
-            
-            
-        
+        private static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
         public void Render(ScriptableRenderContext context, Camera camera)
         {
             this._camera = camera;
             this._context = context;
+            PrepareBuffer();
+            PrepareForSceneWindow();
             if (!Cull()) return;
             Setup();
             DrawVisibleGeomerty();
+            DrawUnsupportedShaders();
+            DrawGizmos(); //绘制相机视椎
             Submit();
         }
 
         void DrawVisibleGeomerty()
         {
+            //通过排序后的相机的正交排序或者距离的排序来平判断是否渲染
+            var sortingSettings = new SortingSettings(_camera) {
+                criteria = SortingCriteria.CommonOpaque
+            };
+            var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+            //将渲染队列中的所有对象设置为过滤的对象
+            var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+            _context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
             _context.DrawSkybox(_camera);
+            sortingSettings.criteria = SortingCriteria.CommonTransparent;
+            drawingSettings.sortingSettings = sortingSettings;
+            filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+            _context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
         }
         void Setup()
         {            
             _context.SetupCameraProperties(_camera);
-            buffer.ClearRenderTarget(true, true, Color.clear);
-            buffer.BeginSample(bufferName);
+            CameraClearFlags flags = _camera.clearFlags;
+            buffer.ClearRenderTarget(
+                flags <= CameraClearFlags.Depth, 
+                flags == CameraClearFlags.Color,
+                flags == CameraClearFlags.Color ? _camera.backgroundColor.linear : Color.clear
+                );
+            buffer.BeginSample(SampleName);
             ExecuteBuffer();
         }
         void Submit()
         {
-            buffer.EndSample(bufferName);
+            buffer.EndSample(SampleName);
             ExecuteBuffer();
             _context.Submit();
         }
